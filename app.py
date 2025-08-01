0619275103d75c9c96b87a171da9ceda6d70ee61
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-08-01 13:09:07 +0700

    p

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import re
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -102,5 +102,8 @@ def chat():
     answer = ask_chatbot(user_input)
     return jsonify({"reply": answer})
 
+
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
